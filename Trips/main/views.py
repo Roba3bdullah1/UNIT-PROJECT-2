6707e42d9a6fb4ff687d43ccd91216faa2287f93
Roba Abdullah <roba3bdullah@hotmail.com>
2025-07-25 15:01:50 +0300

    Updated

--- Trips/main/views.py
+++ Trips/main/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from trip.models import Trip
 from trip.forms import TripForm
+import trip.views
 
 
 def home_view(request:HttpRequest):
     trips = Trip.objects.order_by('-date')[0:3]
-    return render(request, 'main/home.html', {"trips": trips})
+    categories = Trip.Category.choices
+
+    context = {
+        'trips': trips,
+        'categories':categories,
+    }
+    return render(request, 'main/home.html',context)
+
 
 def contact_view(request:HttpRequest):
 
-    return render(request, 'main/contact.html' )
+    return render(request, 'main/contact.html',)
 
